ca31982563529e44e396169c55f4a85d5feb5026
Bence Mitlasoczki <mitlabence111@gmail.com>
2025-08-20 16:20:20 +0200

    Add CSV content decoding (copy contents into text input) functionality

--- chrolispp_planner.py
+++ chrolispp_planner.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from math import floor
 
+
 class LEDIndexError(Exception):
     """Exception raised when LED index is out of range."""
 
@@ -11,6 +12,7 @@ class LEDIndexError(Exception):
 
     def __str__(self):
         return f"{self.message}"
+
 
 class InvalidParameterError(Exception):
     """Exception raised when generally a parameter is invalid."""
@@ -22,11 +24,24 @@ class InvalidParameterError(Exception):
     def __str__(self):
         return f"{self.message}"
 
+
+class InvalidLineError(Exception):
+    """Exception raised when a CSV line is invalid."""
+
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message}"
+
+
 LED_INDEX_LABEL = "LED index (1-6)"
 FREQUENCY_LABEL = "Frequency (Hz)"
 TOTAL_DURATION_LABEL = "Total Duration (s)"
 PULSE_DURATION_LABEL = "Pulse Duration (ms)"
 POWER_LABEL = "Power (0-1000=0-100.0%)"
+
 
 class CSVApp:
     def __init__(self, root):
@@ -35,7 +50,13 @@ class CSVApp:
 
         # Input fields
         self.entries = {}
-        parameters = [LED_INDEX_LABEL, FREQUENCY_LABEL, TOTAL_DURATION_LABEL, PULSE_DURATION_LABEL, POWER_LABEL]
+        parameters = [
+            LED_INDEX_LABEL,
+            FREQUENCY_LABEL,
+            TOTAL_DURATION_LABEL,
+            PULSE_DURATION_LABEL,
+            POWER_LABEL,
+        ]
         for i, label in enumerate(parameters):
             tk.Label(root, text=label).grid(row=0, column=i)
             entry = tk.Entry(root, width=10)
@@ -46,49 +67,99 @@ class CSVApp:
         plus_button.grid(row=1, column=len(parameters), padx=10)
 
         remove_button = tk.Button(root, text="-", command=self.remove_last_line)
-        remove_button.grid(row=1, column=len(parameters)+1, padx=10)
+        remove_button.grid(row=1, column=len(parameters) + 1, padx=10)
 
+        self.output_title = tk.Label(root, text="Output").grid(
+            row=2, column=0, columnspan=len(parameters)
+        )
         self.output_text = tk.Text(root, height=10, width=50)
-        self.output_text.grid(row=2, column=0, columnspan=4, pady=10)
+        self.output_text.grid(row=3, column=0, columnspan=len(parameters), pady=10)
         self.output_text.config(state=tk.DISABLED)
 
+        self.input_title = tk.Label(root, text="Input").grid(
+            row=4, column=0, columnspan=len(parameters)
+        )
+        self.input_text = tk.Text(root, height=10, width=50)
+        self.input_text.grid(row=5, column=0, columnspan=len(parameters), pady=10)
+
+        validate_button = tk.Button(root, text="Validate", command=self.validate_entry)
+        validate_button.grid(row=5, column=len(parameters) + 1, padx=10)
+
+        # Validation result output field
+        self.validation_output = tk.Text(root, height=20, width=120)
+        self.validation_output.grid(row=6, column=0, columnspan=len(parameters), pady=10)
+        self.validation_output.config(state=tk.DISABLED)
+        
         # Store lines
         self.csv_lines = []
 
     def get_line(self):
-            # Get and validate input values
-            led_index = int(self.entries[LED_INDEX_LABEL].get())
-            if led_index < 1 or led_index > 6:
-                raise LEDIndexError(f"Invalid LED index: {led_index}.")
-            led_index -= 1  # convert to 0-5
-            freq = float(self.entries[FREQUENCY_LABEL].get())
-            total = float(self.entries[TOTAL_DURATION_LABEL].get())
-            pulse_duration_ms = int(self.entries[PULSE_DURATION_LABEL].get())
-            power = int(self.entries[POWER_LABEL].get())
-            total_duration_ms = int(floor(total*1000.0))
-            # detect break:
-            #   power should be 0
-            if power == 0:
-                line = f"{led_index},0,{total_duration_ms},1,0"
-                return line
-            # check pulse duration is not too short or too long
-            #   too short when < 1 ms (0 ms)
-            cycle_duration_ms = int(floor(1000.0/freq))  # in milliseconds
-            if pulse_duration_ms < 1 or pulse_duration_ms > cycle_duration_ms:
-                raise InvalidParameterError(f"Pulse duration invalid: should be 0 < {pulse_duration_ms} <= {cycle_duration_ms} ms")
-            if power < 0 or power > 1000:
-                raise InvalidParameterError(f"Power invalid: should be 0 <= {power} <= 1000")
-            if freq == 0:
-                raise InvalidParameterError(f"Frequency invalid: 0 Hz for non-break (power != 0) segment.")
-            # Calculate parameters
-            break_duration_ms = cycle_duration_ms - pulse_duration_ms
-            assert break_duration_ms >= 0 
-            n_cycles = int(total_duration_ms//cycle_duration_ms) 
-
-            # Create CSV-compatible line
-            line = f"{led_index},{pulse_duration_ms},{break_duration_ms},{n_cycles},{power}"
+        """Read out the input fields and return a chrolispp-compatible csv line"""
+        # Get and validate input values
+        led_index = int(self.entries[LED_INDEX_LABEL].get())
+        if led_index < 1 or led_index > 6:
+            raise LEDIndexError(f"Invalid LED index: {led_index}.")
+        led_index -= 1  # convert to 0-5
+        freq = float(self.entries[FREQUENCY_LABEL].get())
+        total = float(self.entries[TOTAL_DURATION_LABEL].get())
+        pulse_duration_ms = int(self.entries[PULSE_DURATION_LABEL].get())
+        power = int(self.entries[POWER_LABEL].get())
+        total_duration_ms = int(floor(total * 1000.0))
+        # detect break:
+        #   power should be 0
+        if power == 0:
+            line = f"{led_index},0,{total_duration_ms},1,0"
             return line
-    
+        # check pulse duration is not too short or too long
+        #   too short when < 1 ms (0 ms)
+        cycle_duration_ms = int(floor(1000.0 / freq))  # in milliseconds
+        if pulse_duration_ms < 1 or pulse_duration_ms > cycle_duration_ms:
+            raise InvalidParameterError(
+                f"Pulse duration invalid: should be 0 < {pulse_duration_ms} <= {cycle_duration_ms} ms"
+            )
+        if power < 0 or power > 1000:
+            raise InvalidParameterError(
+                f"Power invalid: should be 0 <= {power} <= 1000"
+            )
+        if freq == 0:
+            raise InvalidParameterError(
+                f"Frequency invalid: 0 Hz for non-break (power != 0) segment."
+            )
+        # Calculate parameters
+        break_duration_ms = cycle_duration_ms - pulse_duration_ms
+        assert break_duration_ms >= 0
+        n_cycles = int(total_duration_ms // cycle_duration_ms)
+
+        # Create CSV-compatible line
+        line = f"{led_index},{pulse_duration_ms},{break_duration_ms},{n_cycles},{power}"
+        return line
+
+    def decode_line(self, line: str):
+        """Take a csv line and decode it into a Chrolispp step"""
+        entries = line.strip().split(",")
+        if (
+            len(entries) != 5
+        ):  # should have LED index, pulse duration, break duration, #pulses, LED power
+            raise InvalidLineError(
+                f"The following line is invalid (!= 5 columns): {line}"
+            )
+        led_index = int(entries[0])+1
+        pulse_duration_ms = int(entries[1])
+        break_duration_ms = int(entries[2])
+        n_pulses = int(entries[3])
+        power = int(entries[4])
+        if power == 0:  # break
+            return f"Break duration: {break_duration_ms/1000.} s"
+        # Calculate total duration
+        cycle_duration_ms = pulse_duration_ms + break_duration_ms
+        total_duration_ms = n_pulses*cycle_duration_ms
+        # Calculate frequency
+        freq = 1000.0/cycle_duration_ms
+        # format power
+        power_str = str(power/10.0)
+        return f"LED {led_index}, frequency: {freq:.1f} Hz, total duration: {total_duration_ms/1000.:.1f} s, pulse duration: {pulse_duration_ms} ms, power: {power_str} %"
+
+
     def add_line(self):
         try:
             line = self.get_line()
@@ -99,7 +170,10 @@ class CSVApp:
             self.output_text.insert(tk.END, "\n".join(self.csv_lines))
             self.output_text.config(state=tk.DISABLED)
         except ValueError:
-            messagebox.showerror("Invalid input", "Please enter valid integers in all fields.")
+            messagebox.showerror(
+                "Invalid input", "Please enter valid integers in all fields."
+            )
+
     def remove_last_line(self):
         if self.csv_lines:
             self.csv_lines.pop()
@@ -109,6 +183,19 @@ class CSVApp:
             self.output_text.config(state=tk.DISABLED)
         else:
             messagebox.showinfo("No lines", "There are no lines to remove.")
+
+    def validate_entry(self):
+        user_input = self.input_text.get("1.0", tk.END).strip().split("\n")
+        decoded_lines = []
+        for i_line, line in enumerate(user_input):
+            if len(line) == 0:
+                continue
+            decoded_line = self.decode_line(line)
+            decoded_lines.append(f"Step {i_line+1}: {decoded_line}")
+        self.validation_output.config(state=tk.NORMAL)
+        self.validation_output.delete("1.0", tk.END)
+        self.validation_output.insert(tk.END, "\n".join(decoded_lines))
+        self.validation_output.config(state=tk.DISABLED)
 
 # Run the app
 if __name__ == "__main__":
